ec0630f110d6bcb77b9872e23b6fe3366aa9ed3b
RobCar1987 <roberto.caridi@gmail.com>
2025-05-05 22:10:09 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,7 @@
 module.exports = ({ env }) => ({
-  "users-permissions": {
+  'users-permissions': {
     config: {
-      jwtSecret: env("JWT_SECRET"),
-    },
-  },
-  upload: {
-    config: {
-      provider: "local",
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
+      jwtSecret: env('JWT_SECRET'),
     },
   },
 });
